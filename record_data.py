7c698a6b0e2ae9ac1d252f9cd1d060bd881fca68
MozhouGao <73396800+MozhouGao@users.noreply.github.com>
2023-03-05 17:37:27 -0700

    Add files via upload

---
+++ record_data.py
@@ -0,0 +1,56 @@
+import csv 
+import time 
+import numpy as np
+import pandas as pd 
+import geopandas as gpd
+
+gps = pd.read_csv('GPS.csv',sep=',')
+
+times = gps.time
+lats = np.array(gps.latitude)
+lons = np.array(gps.longitude)
+
+fieldnames = ['lons','lats','time']
+
+
+with open ('mydesktop.csv','w') as csv_file: 
+    csv_writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+    csv_writer.writeheader()
+
+Day = True
+i = 1  
+while Day:
+        
+    with open('mydesktop.csv', 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        lat = lats[i]
+        lon = lons[i]
+        hour_a_day = times[i]
+        
+        
+        tdf = pd.DataFrame(data={'lat':[lat],'lon':[lon]})
+        tgdf = gpd.GeoDataFrame(tdf, geometry=gpd.points_from_xy(tdf.lon, tdf.lat))
+        tgdf = tgdf.set_crs("EPSG:4326")
+        tgdf.geometry = tgdf.geometry.to_crs(epsg=26910)
+
+        
+        lat_value = tgdf.geometry.y.iloc[0]
+        lon_value = tgdf.geometry.x.iloc[0]
+        hour = hour_a_day
+        
+        
+        info = {
+            "lons": lon_value,
+            "lats": lat_value,
+            "time": hour
+        }
+
+        csv_writer.writerow(info)
+        
+        
+        print(lat_value, lon_value, hour)
+    i += 1        
+    time.sleep(1)
+    
+    
+    
